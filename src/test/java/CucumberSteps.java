413a5addfb23d36890031731a8252e68eef7e4c5
op.gg <op@gmail>
2024-10-11 13:10:40 +0600

    added image

--- src/test/java/CucumberSteps.java
+++ src/test/java/CucumberSteps.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 
+import java.nio.file.Paths;
+
 public class CucumberSteps {
     WebDriver webDriver ;
     private boolean successMsg ;
@@ -22,8 +24,9 @@ public class CucumberSteps {
 
     @When("Upload File {string}")
     public void uploadFile(String location) {
+        String absolutePath = Paths.get(System.getProperty("user.dir"), location).toAbsolutePath().toString();
         WebElement  inputBtn = webDriver.findElement(By.id("file-upload"));
-        inputBtn.sendKeys(location);
+        inputBtn.sendKeys(absolutePath);
         WebElement uploadBtn = webDriver.findElement(By.id("file-submit"));
         uploadBtn.click();
         WebElement uploadMsg = webDriver.findElement(By.xpath("//h3[contains(text(),'File Uploaded!')]"));
--- src/test/resources/cucumber.feature
+++ src/test/resources/cucumber.feature
@@ -3,7 +3,7 @@ Feature: Checking file Upload
   Background:
   Scenario: UploadFile & Check Result
     Given Going To Url
-    When Upload File "C:\\Users\\m2.hasan\\Downloads\\Date Validitioni.PNG"
+    When Upload File "src/test/resources/Date Validitioni.PNG"
     Then Check Message
 
 
